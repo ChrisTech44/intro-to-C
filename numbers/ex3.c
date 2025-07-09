0ef54dc74e23419294d51bdfc96525a88fb5fe23
Christian Franklin <93060146+ChrisTech44@users.noreply.github.com>
2025-07-09 16:59:32 -0400

    Finished numbers exercise files

---
+++ numbers/ex3.c
@@ -0,0 +1,14 @@
+
+/*
+* An example
+*/
+int main() {
+    //Do not touch anything BEFORE this line
+    double x = '3.2';
+
+    char y = %;
+
+    int z = 5
+
+    //Do not touch anything AFTER this lne
+}
---
+++ numbers/numbers.c
@@ -0,0 +1,16 @@
+
+/*
+* A simple demonstration of declaring integers and floating point numbers
+*/
+int main() {
+    /* Declare x as an integer and make its value 8*/
+    int x = 8;
+    
+    /* Declare y as an double and make its value 1.3 */
+    double y = 1.3;
+
+    /* Declare letter as a character and make its value A*/
+    char letter = 'A';
+    letter = '%'; /* Change letter to represent '%', since it has 
+    already been declared, you do not need to specify char again*/
+}
